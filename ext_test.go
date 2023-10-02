1b5d631bfb085cc4fdf8def6438569c761598225
Dennis Trautwein <git@dtrautwein.eu>
2023-10-02 16:15:45 +0200

    Squashed 'go-libp2p-kad-dht/' content from commit b63ad60
    git-subtree-dir: go-libp2p-kad-dht
    git-subtree-split: b63ad6096833d36b365f1361edab871f6cdc283c

---
+++ ext_test.go
@@ -0,0 +1,46 @@
+package dht
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/stretchr/testify/require"
+
+	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
+)
+
+func TestInvalidRemotePeers(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	mn, err := mocknet.FullMeshLinked(5)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer mn.Close()
+	hosts := mn.Hosts()
+
+	os := []Option{testPrefix, DisableAutoRefresh(), Mode(ModeServer)}
+	d, err := New(ctx, hosts[0], os...)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, proto := range d.serverProtocols {
+		// Hang on every request.
+		hosts[1].SetStreamHandler(proto, func(s network.Stream) {
+			defer s.Reset() // nolint
+			<-ctx.Done()
+		})
+	}
+
+	err = mn.ConnectAllButSelf()
+	if err != nil {
+		t.Fatal("failed to connect peers", err)
+	}
+
+	time.Sleep(100 * time.Millisecond)
+
+	require.Equal(t, 0, d.routingTable.Size())
+}
