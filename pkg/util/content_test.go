b422d434c8f28b79e5730ec1a907579b136381c5
Dennis Trautwein <git@dtrautwein.eu>
2025-09-15 03:09:35 +0200

    fix: usage of new dependencies

--- pkg/util/content_test.go
+++ pkg/util/content_test.go
@@ -3,8 +3,8 @@ package util
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"gotest.tools/v3/assert"
 )
 
 func TestNewRandomContent(t *testing.T) {
