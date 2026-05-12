50256af0f8bf5361528f06976a35307714b4ebe0
Dennis Trautwein <git@dtrautwein.eu>
2026-05-12 14:52:43 +0200

    chore: extend configurability

--- pkg/server/server_handlers.go
+++ pkg/server/server_handlers.go
@@ -96,52 +96,6 @@ func (s *Server) retrieve(rw http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	//resp := RetrievalResponse{
-	//	CID:              c.String(),
-	//	RoutingTableSize: RoutingTableSize(s.host.DHT),
-	//}
-	//logEntry := log.WithField("cid", c.String()).WithField("rtSize", resp.RoutingTableSize)
-	//
-	//logEntry.Infoln("Start finding providers")
-	//
-	//// here's where the magic happens
-	//switch rr.Routing {
-	//case config.RoutingIPNI:
-	//	start := time.Now()
-	//	pr, err := s.host.IndexerLookup(ctx, c)
-	//	resp.Duration = time.Since(start)
-	//
-	//	logEntry = logEntry.WithField("dur", resp.Duration.Seconds())
-	//	if err != nil {
-	//		logEntry.Warnln("Failed looking up provider")
-	//		resp.Error = err.Error()
-	//	} else {
-	//		if len(pr.MultihashResults) == 0 {
-	//			resp.Error = "not found"
-	//		}
-	//	}
-	//	latencies.WithLabelValues("retrieval_ttfpr", string(config.RoutingIPNI), strconv.FormatBool(resp.Error == ""), r.Header.Get(headerSchedulerID)).Observe(resp.Duration.Seconds())
-	//case config.RoutingDHT:
-	//	start := time.Now()
-	//	provider := <-s.host.DHT.FindProvidersAsync(ctx, c, 1)
-	//	resp.Duration = time.Since(start)
-	//
-	//	logEntry = logEntry.WithField("dur", resp.Duration.Seconds())
-	//
-	//	if errors.Is(provider.ID.Validate(), peer.ErrEmptyPeerID) {
-	//		resp.Error = "not found"
-	//		logEntry.Infoln("Didn't find provider")
-	//	} else {
-	//		s.host.Network().ClosePeer(provider.ID)
-	//		s.host.Peerstore().RemovePeer(provider.ID)
-	//		s.host.Peerstore().ClearAddrs(provider.ID)
-	//		logEntry.WithField("provider", util.FmtPeerID(provider.ID)).Infoln("Found provider")
-	//	}
-	//	latencies.WithLabelValues("retrieval_ttfpr", string(config.RoutingDHT), strconv.FormatBool(resp.Error == ""), r.Header.Get(headerSchedulerID)).Observe(resp.Duration.Seconds())
-	//default:
-	//	panic(fmt.Sprintf("unknown routing: %s", rr.Routing))
-	//}
-
 	data, err = json.Marshal(resp)
 	if err != nil {
 		http.Error(rw, err.Error(), http.StatusInternalServerError)
